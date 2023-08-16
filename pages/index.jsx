fe3f03b524cf51d9d4113c7050b9ec9d93548cf5
itsbrex <roach7@gmail.com>
2023-08-16 10:26:02 -0700

    fdgdsfg

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,9 @@ import Layout from '../components/Layout'
 export default function Home() {
   const headProps = {
     title:
-      'Descript - Seamless YouTube Video and Transcript Importing',
+      'Descript - Seamless YouTube Video Transcript Importing',
     description:
-      'Paste a YouTube link to directly import videos and automatically transcribe them. Save time and increase efficiency with Descript.',
+      'Paste a YouTube link to automatically get transcripts and other video info. Save time and move faster with Descript.',
     keywords: ['Descript', 'YouTube', 'import', 'transcription', 'video', 'subtitle', 'audio', 'clip', 'downloader', 'efficiency', 'content creation'],
     image:
       'https://user-images.githubusercontent.com/1222611/92627876-a85ee000-f2d4-11ea-8dbb-14ab4eb0065a.png'
