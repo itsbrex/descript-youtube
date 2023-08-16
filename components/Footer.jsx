fe3030dfb271a48a1f9159fc3c3864d0886d73f4
itsbrex <roach7@gmail.com>
2023-08-16 10:24:27 -0700

    update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,17 +2,17 @@ import { IconButton } from '@material-ui/core'
 import { GitHub as GitHubIcon } from '@material-ui/icons'
 
 const Footer = () => {
-	return (
-		<footer style={{ flex: 1, maxHeight: 100 }}>
-			<IconButton
-				href='https://github.com/aparus/direct-link'
-				target='_blank'
-				rel='noopener noreferrer'
-			>
-				<GitHubIcon />{' '}
-			</IconButton>
-		</footer>
-	)
+  return (
+    <footer style={{ flex: 1, maxHeight: 100 }}>
+      <IconButton
+        href='https://github.com/itsbrex/DescriptTube'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <GitHubIcon />{' '}
+      </IconButton>
+    </footer>
+  )
 }
 
 export default Footer
