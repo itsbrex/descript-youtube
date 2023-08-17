a86ef755be1aceb48ebf3bc071c03ff18356bed4
itsbrex <roach7@gmail.com>
2023-08-17 01:26:04 -0700

    style: Update VideoArticle component JSX formatting and class names

--- components/VideoArticle.jsx
+++ components/VideoArticle.jsx
@@ -1,60 +1,57 @@
-import { Typography } from '@mui/material'
 import SubtitleFileLinks from './video/SubtitleFileLinks'
 import Keywords from './video/Keywords'
 import Video from './video/Video'
 import Thumbnails from './video/Thumbnails'
 
 const VideoArticle = props => {
-	const {
-		title,
-		keywords,
-		captionTracks,
-		urlVideo,
-		urlAudio,
-		thumbnails,
-		description,
-		image: poster
-	} = props
-	return (
-		<article style={{ marginTop: 30 }}>
-			<Typography style={{ fontSize: 40, textAlign: 'center' }} variant='h1'>
-				{title}
-			</Typography>
-			<p />
-			<Keywords keywords={keywords} />
-			<p />
-			<Video
-				captionTracks={captionTracks}
-				urlVideo={urlVideo}
-				poster={poster}
-			/>
-			<p />
-			<audio controls src={urlAudio} style={{ width: '100%' }} />
-			<p />
-			<Typography style={{ fontSize: 20 }} variant='h3'>
-				Captions/Subtitles:
-			</Typography>
-			<SubtitleFileLinks captionTracks={captionTracks} />
-			<p />
-			<Typography style={{ fontSize: 20 }} variant='h3'>
-				Thumbnails:
-			</Typography>
-			<p />
-			<Thumbnails thumbnails={thumbnails} />
-			<p />
-			<Typography style={{ fontSize: 20 }} variant='h3'>
-				Description:
-			</Typography>
-			<p />
-			<Typography
-				style={{ whiteSpace: 'pre-line', overflowWrap: 'break-word' }}
-				variant='body2'
-			>
-				{description}
-			</Typography>
-			<p />
-		</article>
-	)
+  const {
+    title,
+    keywords,
+    captionTracks,
+    urlVideo,
+    urlAudio,
+    thumbnails,
+    description,
+    image: poster
+  } = props
+  return (
+    <article className="mt-8">
+      <h1 className="text-4xl text-center">
+        {title}
+      </h1>
+      <div className="my-3" />
+      <Keywords keywords={keywords} />
+      <div className="my-3" />
+      <Video
+        captionTracks={captionTracks}
+        urlVideo={urlVideo}
+        poster={poster}
+      />
+      <div className="my-3" />
+      <audio controls src={urlAudio} className="w-full" />
+      <div className="my-3" />
+      <h3 className="text-2xl">
+        Captions/Subtitles:
+      </h3>
+      <SubtitleFileLinks captionTracks={captionTracks} />
+      <div className="my-3" />
+      <h3 className="text-2xl">
+        Thumbnails:
+      </h3>
+      <div className="my-3" />
+      <Thumbnails thumbnails={thumbnails} />
+      <div className="my-3" />
+      <h3 className="text-2xl">
+        Description:
+      </h3>
+      <div className="my-3" />
+
+      <p className="whitespace-pre-line break-words">
+        {description}
+      </p>
+      <div className="my-3" />
+    </article>
+  )
 }
 
 export default VideoArticle
