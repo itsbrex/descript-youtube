76832f18b7484d944b678f3de3b18681e113c73f
itsbrex <roach7@gmail.com>
2023-08-17 01:52:35 -0700

    refactor: Remove commented out code in SubtitleFileLinks.jsx

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,26 +1,13 @@
 import Search from '../components/Search'
 import Logo from './Logo'
 import DescriptionIcons from './DescriptionIcons'
-import { Typography } from '@mui/material'
 
 const Heading = () => {
   return (
-    <header style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flex: 1,
-      width: '100%',
-      marginTop: 0
-    }}
-    >
-      <Typography
-        variant='h1'
-        style={{ fontSize: 35, width: '100%', textAlign: 'center' }}
-      >
+    <header className="flex flex-col justify-center items-center flex-1 w-full mt-0">
+      <h1 className="text-4xl w-full text-center">
         <Logo text='YouTube' /> Descript
-      </Typography>
+      </h1>
       <DescriptionIcons />
       <Search />
     </header>
